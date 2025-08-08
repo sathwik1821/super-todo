650027bc9815403dd0380995cc204db1f2a64e46
sathwik1821 <sathwikbodakunta2005@gmail.com>
2025-08-08 15:43:28 +0530

    added completed tasks,delete all,sort all features

--- src/app/TodoPage.jsx
+++ src/app/TodoPage.jsx
@@ -40,9 +40,35 @@ const TodoPage = () => {
     setTodos(newTodos)
   }
 
+  function handleDeleteAll(){
+    setTodos([])
+  }
+
+
+  function compareFunction(a, b) {
+    return a.text.localeCompare(b.text);
+  }
+
+
+  function handleSortTodos(){
+    const newTodos=[...todos];
+    newTodos.sort(compareFunction);
+    setTodos(newTodos);
+  }
+  
+
   const emptyState=<h2>No Tasks Added...Add Now...</h2>
 
+  function isSorted(arr, compareFn) {
+    return arr.every((value, index, array) => {
+      return index === 0 || compareFn(array[index - 1], value) <= 0;
+    });
+  }
+
+
   const totalTodos=todos.length;
+
+  const completedTodos=todos.filter((item)=>item.completed);
 
   return (
     
@@ -56,7 +82,11 @@ const TodoPage = () => {
         {totalTodos==0 ? emptyState :
 
           <div>
+            {!isSorted(todos,compareFunction) && <button type='button' onClick={handleSortTodos}>Sort All</button>}
+            <button  type="button" onClick={handleDeleteAll}>Delete All</button>
+            <div>{`${completedTodos.length} tasks out of ${totalTodos} tasks completed`}</div>
           {
+            
             todos.map((item)=>
             (
                 <TodoItem key={item.id} item={item}
@@ -67,8 +97,6 @@ const TodoPage = () => {
           }
           </div>
         }
-        
-        
       </form>
     </div>
   )
